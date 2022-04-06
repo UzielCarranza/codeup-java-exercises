50b5339b7ce2e8c630e1e153eed10e085e7f5741
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-06 11:58:26 -0400

    variables

---
+++ src/SyntaxTypes.java
@@ -0,0 +1,31 @@
+public class SyntaxTypes {
+
+    public static void main(String[] args) {
+
+
+
+        byte daysOfMonth = 31;
+
+        short employees = 1000;
+
+        int userID = 9188;
+
+        //Needs l to specify its long value
+        long circMilkWay = 10213133130L;
+
+//        decimals
+        float roundedPi = 3.14F;
+
+        double someBigDecimal = 3.141689;
+
+
+//
+        char yesNo = 'Y';
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+
+
+    }
+}
