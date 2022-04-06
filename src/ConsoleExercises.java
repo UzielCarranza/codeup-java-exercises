07808a6232dff619c01c8a4e8293e618e1529aa0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-06 16:06:43 -0400

    completed exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,11 +6,11 @@ public class ConsoleExercises {
         Scanner scanner = new Scanner(System.in);
 
 
-
-
-
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately: " + "%.2f", pi);
+//
+//
+//
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately: " + "%.2f", pi);
 
 
 
@@ -18,13 +18,38 @@ public class ConsoleExercises {
 //
 //        What happens if you input something that is not an integer?
         //system wil fail
+//
+//        String newline = System.lineSeparator();
+//
+//        System.out.println(newline + "Please enter your age :)");
+//        int userAge = scanner.nextInt();
+//
+//        System.out.println("your age is: " + userAge);
 
-        String newline = System.lineSeparator();
 
-        System.out.println(newline + "Please enter your age :)");
-        int userAge = scanner.nextInt();
 
-        System.out.println("your age is: " + userAge);
+
+
+//        Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
+//
+//                What happens if you enter less than 3 words?
+//                What happens if you enter more than 3 words?
+
+
+        System.out.println("please enter 1 word");
+        String string1 = scanner.next();
+
+
+        System.out.println("please enter another word");
+        String string2 = scanner.next();
+
+
+        System.out.println("please enter last word");
+        String string3 = scanner.next();
+
+
+        System.out.printf(" here are your inputs!: %s, %s, %s", string1, string2, string3 );
+
 
     }
 }
