6257b6509c9619ad98a3f4464e776b57f4e3b112
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-07 15:43:46 -0400

    starting code to be more user friendly

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+
+        boolean continueRunningApp = true;
+
+
+        System.out.println("Hey there, I am Bob... What can I do for you today?");
+
+        String userInput = input.nextLine();
+        if (userInput.endsWith("?")) {
+
+            System.out.println("Sure...");
+
+        } else if (userInput.endsWith("!")) {
+
+            System.out.println("Whoa, chill out!");
+        } else {
+            System.out.println(" Fine. Be that way!");
+        }
+
     }
+
 }
+
+
+
