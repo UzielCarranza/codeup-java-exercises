ae8c0e1dece2faa125c882c283c234b8368bbd9a
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-07 17:54:19 -0400

    created method to sum nums

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+    int num1;
+    int num2;
+
+    public Addition(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+
+    }
+    public int fsumNums(){
+        return (this.num1 + this.num2);
+    }
+
+}
