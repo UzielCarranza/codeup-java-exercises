ab2ebe8c25a4a95c28a7cc397678efbb5428a847
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-08 14:38:22 -0400

    started code

--- src/MethodsExercisesWhileLoops.java
+++ src/MethodsExercisesWhileLoops.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class MethodsExercisesWhileLoops {
+;public class MethodsExercisesWhileLoops {
+//    boolean keepRunnignApplicationAddition;
+//    int num2;
+//
+//    public  Division(int num1, int num2){
+//        this.num1 = num1;
+//        this.num2 = num2;
+//
+//    }
+//    public int divideNums(){
+//        return this.num1 / this.num2;
+//    }
 }
