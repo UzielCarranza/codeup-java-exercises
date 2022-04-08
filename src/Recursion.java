caed173a54ee2a37a71d080eb1097a968ea2c8f6
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-08 10:30:23 -0400

    added recursion

---
+++ src/Recursion.java
@@ -0,0 +1,14 @@
+public class Recursion {
+    public static void count(int n) {
+        if (n <= 0) {
+            System.out.println("All done!");
+            return;
+        }
+        System.out.println(n);
+        count(n - 1);
+    }
+
+    public static void main(String[] args) {
+        count(5);
+    }
+}
