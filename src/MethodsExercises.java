96dc1ae03e759996c6a172fd4744de9615ce83f8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-07 21:03:26 -0400

    completed subtraction

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,13 +4,13 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
 
-
-        System.out.println("let's sdo some addition!");
         Scanner input = new Scanner(System.in);
+
+
+//        addition
+        System.out.println("let's sdo some addition!");
         System.out.println("Enter first number");
         int firstNum = input.nextInt();
-
-
 
         System.out.println("Enter second number");
         int secondNum = input.nextInt();
@@ -20,6 +20,25 @@ public class MethodsExercises {
         int totalAddition1 = addition1.sumNums();
 
         System.out.println(totalAddition1);
+//        ends addition
+
+
+
+//        starts subtraction
+        System.out.println("let's sdo some subtraction!");
+        System.out.println("Enter first number");
+        int subtractionNum1 = input.nextInt();
+
+        System.out.println("Enter second number");
+        int subtractionNum2 = input.nextInt();
+        Subtraction subtraction1 = new Subtraction(subtractionNum1, subtractionNum2);
+        int totalSubtraction = subtraction1.subtractNums();
+        System.out.println(totalSubtraction);
+//        ends subtraction
+
+
+
+
     }
 
 }
---
+++ src/Subtraction.java
@@ -0,0 +1,15 @@
+public class Subtraction {
+        int num1;
+        int num2;
+
+        public  Subtraction(int num1, int num2){
+            this.num1 = num1;
+            this.num2 = num2;
+
+        }
+        public int subtractNums(){
+            return this.num1 - this.num2;
+        }
+
+
+}
