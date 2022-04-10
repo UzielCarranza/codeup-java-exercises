8947a4c1e44ba7f7f45ba28e3b60fd180d89e554
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-10 19:49:50 -0400

    ciompleted setting up constructor

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,21 @@
 public class Person {
+    private String name;
+
+    public String getName() {
+//        this.name = name;
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name) {
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+
+    public void sayHello() {
+//TODO: print a message to the console using the person's name
+        System.out.println(this.name);
+    }
+
+
 }
