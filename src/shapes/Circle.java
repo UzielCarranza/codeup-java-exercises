7f33fc3be708fd9d38a503ff1eaf64644c20f8da
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-11 18:28:43 -0400

    completed first exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,25 @@
 package shapes;
 
+import static java.lang.Math.PI;
+
 public class Circle {
+    //    fields
+    private double radious;
+
+    //methods
+    public Circle(double radius) {
+        this.radious = radius;
+        double circumference = 2 * (Math.abs(PI) * radius);
+        System.out.println(circumference);
+    }
+
+    public double getArea() {
+
+        return 0;
+    }
+
+    public double getCircumference() {
+
+        return 0;
+    }
 }
