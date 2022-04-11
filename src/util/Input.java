16cd68ca1e7fa72ed5bdd29ea5f386220f3c26ef
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-10 20:27:32 -0400

    started exercise

---
+++ src/util/Input.java
@@ -0,0 +1,39 @@
+package util;
+
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    String getString(){
+
+        return null;
+    }
+
+    boolean yesNo(){
+
+        return false;
+    }
+
+    int getInt(int min, int max){
+
+        return min;
+    }
+
+    int getInt(){
+
+        return 0;
+    }
+
+    double getDouble(double min, double max){
+
+        return min;
+    }
+
+    double getDouble(){
+
+        return 0;
+    }
+
+}
