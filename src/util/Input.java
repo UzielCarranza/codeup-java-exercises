c9e8a157151f0fc1a5de3a3e925a03c516c81519
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-11 16:30:47 -0400

    completed input classes

--- src/util/Input.java
+++ src/util/Input.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class Input {
     //    fields
     private Scanner scanner = new Scanner(System.in);
+    private int min;
+    private int max;
+    private double minDouble;
+    private double maxDouble;
 
 
-//    methods
+
+//    methods for strings
 
     String getString(Scanner userInput) {
         this.scanner = userInput;
@@ -18,24 +23,37 @@ public class Input {
         return getString(this.scanner).equalsIgnoreCase("yes") || getString(this.scanner).equalsIgnoreCase("y");
     }
 
-    int getInt(int min, int max) {
+//    methods for numbers
 
-        return min;
+    int getInt(int min, int max) {
+        this.min = min;
+        this.max = max;
+        if (min < 0 && min > max) {
+            getInt(this.min, this.max);
+            System.out.println("needs to be within range range");
+        }
+        return getInt();
     }
 
     int getInt() {
+        return getInt(this.min, this.max);
 
-        return 0;
     }
 
     double getDouble(double min, double max) {
+        this.minDouble = min;
+        this.maxDouble = max;
+        if (min < 0 && min > max) {
+            getDouble(this.minDouble, this.maxDouble);
+            System.out.println("needs to be within range");
+        }
+        return getDouble();
 
-        return min;
     }
 
     double getDouble() {
 
-        return 0;
+        return getDouble(this.minDouble, this.maxDouble);
     }
 
 }
