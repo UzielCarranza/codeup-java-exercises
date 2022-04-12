99b4d56149d4d6b0f49adfe8b1609b6510484c8c
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-12 10:13:48 -0400

    set up warm up

--- src/warmups/BackPack.java
+++ src/warmups/BackPack.java
@@ -1,11 +1,30 @@
 package warmups;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class BackPack {
+
+    private Scanner scanner = new Scanner(System.in);
     private boolean lid;
     private double leftStrap;
     private double rightStrap;
 
 
 
+
+    String getString() {
+        System.out.println("Enter yes or no");
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public boolean yesNo() {
+        if (getString().toLowerCase(Locale.ROOT).equalsIgnoreCase("yes")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
