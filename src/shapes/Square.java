6915c385d01f7997a62563383d7b58855dca7609
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-12 18:10:52 -0400

    overrrrride Rectangle mthod setPerimeter

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,6 +5,10 @@ class Square extends Rectangle {
     public Square(int side) {
         this.length = side;
         this.width = side;
+    }
 
+    public void setPerimeter(int length) {
+        this.length = length;
+        this.width = this.length;
     }
 }
