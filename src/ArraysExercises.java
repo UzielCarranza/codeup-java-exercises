268cd2e5a3cf4e723312ac6ad7bc9e0bcef0f429
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-11 20:25:06 -0400

    completed exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,5 +5,16 @@ public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
+
+        String[] Person = {"John", "Paul", "Ringo" };
+        for (String person : Person) {
+            System.out.println(person);
+        }
+
+
     }
+//    public static void addPerson(String [] Person){
+//
+//    }
+
 }
