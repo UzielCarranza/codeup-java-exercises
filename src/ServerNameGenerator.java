f8fcf24696ba2c8a398a8ebe842e150f3e2cc53d
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-11 21:14:12 -0400

    completed exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,18 +1,23 @@
 import java.util.Random;
 
 public class ServerNameGenerator {
-    Object [] array = {"amused", "alert", "angry", "anxious", "attractive", "alive", "annoyed", "Alive", "arrogant", "Average"};
-    Object [] array2 = {"cat", "sock", "ship", "hero", "monkey", "baby", "match", "afternoon", "doctor", "word"};
+    Object[] array = {"amused", "alert", "angry", "anxious", "attractive", "alive", "annoyed", "Alive", "arrogant", "Average" };
+    Object[] array2 = {"cat", "sock", "ship", "hero", "monkey", "baby", "match", "afternoon", "doctor", "word" };
 
-    public Object RandomElementArray(Object [] array1, Object [] array2){
-        this.array = array1;
-        this.array2 = array2;
+    public Object RandomElementArray() {
+
 
         Random generator = new Random();
-        Object[] myArray = new Object[0];
-        int randomIndex = generator.nextInt(myArray.length);
-        return myArray[randomIndex];
+        int randomIndex = generator.nextInt(array.length);
+        int randomIndexArray2 = generator.nextInt(array2.length);
+        String ranArray = array[randomIndex] + "-" + array2[randomIndexArray2];
+        return ranArray;
+
     }
 
+    public static void main(String[] args) {
+        ServerNameGenerator serverNameGenerator = new ServerNameGenerator();
 
+        System.out.println("Here is your server name:\n" +serverNameGenerator.RandomElementArray());
+    }
 }
