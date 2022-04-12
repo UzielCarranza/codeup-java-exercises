ec2853692d96b54fd677c149f25401ddfdff64ac
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-12 18:24:22 -0400

    completed exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -8,8 +8,11 @@ public class ShapesTest {
         System.out.println("Perimeter: " + box1.getPerimeter());
 
 //        Second exercise
-        Rectangle box2 = new Square(5);
-        System.out.println("Area: " + box2.getArea());
+        Square box2 = new Square();
+        box2.setPerimeter(5);
         System.out.println("Perimeter: " + box2.getPerimeter());
+
+        box2.setArea(5);
+        System.out.println("Area: " + box2.getArea());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,6 +2,9 @@ package shapes;
 
 class Square extends Rectangle {
 
+    public Square(){
+
+    }
     public Square(int side) {
         this.length = side;
         this.width = side;
@@ -9,11 +12,11 @@ class Square extends Rectangle {
 
     public void setArea(int length) {
         this.length = length;
-        this.width = this.length;
+        this.width = length;
     }
 
     public void setPerimeter(int length) {
         this.length = length;
-        this.width = this.length;
+        this.width = length;
     }
 }
