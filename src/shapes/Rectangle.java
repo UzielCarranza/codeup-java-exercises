4f9b3778ecd426508fa2af3b46f65692d699ce86
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-12 16:48:29 -0400

    started exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Rectangle {
+    //    fields
+    protected int length;
+    protected int width;
+
+    //    constructor
+    public Rectangle() {
+
+    }
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+
+    }
+}
