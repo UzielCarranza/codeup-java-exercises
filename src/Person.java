9e276cf64ff60bf51255efa5c4e8322e22847ac5
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-12 14:49:55 -0400

    added coments

--- src/Person.java
+++ src/Person.java
@@ -27,14 +27,14 @@ public class Person {
 //        person1.setName("Jose");
 //        person1.sayHello();
 
-
-        Person person1 = new Person();
-        Person person2 = person1;
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-        person2.setName("Jane");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
+//
+//        Person person1 = new Person();
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
     }
 
 }
