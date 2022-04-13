b86ee56276d8d5dd7018275b29073e5f3d2770ae
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-13 17:43:08 -0400

    completed exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,13 +3,16 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("Area: " + box1.getArea());
+        Rectangle box1 = new Rectangle(5, 6);
+        System.out.println("-----------Rectangle details------------");
+        System.out.println("Area" + box1.getArea());
         System.out.println("Perimeter: " + box1.getPerimeter());
 
 //        Second exercise
         Rectangle box2 = new Square(5);
+        System.out.println("-----------Square details------------");
         System.out.println("Perimeter: " + box2.getPerimeter());
         System.out.println("Area: " + box2.getArea());
+        System.out.println("-----------Shutting dowm------------");
     }
 }
