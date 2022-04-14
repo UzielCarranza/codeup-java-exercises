ac966258e3999524fd6bb18d9e17818933b392f0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-14 10:09:30 -0400

    nots for overriding

---
+++ src/warmups/Overrriding.java
@@ -0,0 +1,12 @@
+package warmups;
+
+public class Overrriding {
+    public static void main(String[] args) {
+//        why override?
+
+//        overloading vs overriding
+//        overload: change the method's signature; also can change the method body (the actual code)
+
+//        overriding:
+    }
+}
