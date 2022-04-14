6261df2d7f57c2534570a621380efa1c9860eac0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-14 10:39:33 -0400

    declared getters and setters

--- src/rpgGame/CharacterStructure.java
+++ src/rpgGame/CharacterStructure.java
@@ -21,4 +21,27 @@ public class CharacterStructure {
     }
 
 
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 }
