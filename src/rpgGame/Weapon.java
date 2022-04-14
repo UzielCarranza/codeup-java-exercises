9980c752fb0d48ef636ccdc336700a9b8b685918
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-14 11:32:39 -0400

    deckared weapons class

---
+++ src/rpgGame/Weapon.java
@@ -0,0 +1,21 @@
+package rpgGame;
+
+public class Weapon {
+    private int attack;
+
+    public Weapon(){
+
+    }
+    public Weapon(int attack) {
+        this.attack = attack;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+}
+
