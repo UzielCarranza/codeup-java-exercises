733cb437fae272a0d80c4200977d0d250f79f4fa
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-15 15:21:21 -0400

    set weapons

---
+++ src/gameRpg2/WeaponsApp/Bat.java
@@ -0,0 +1,18 @@
+package gameRpg2.WeaponsApp;
+
+public class Bat extends Weapons {
+    public Bat() {
+
+    }
+
+    public Bat(int weaponDamage) {
+        super(weaponDamage);
+    }
+
+    @Override
+    public int getWeaponDamage() {
+        return super.getWeaponDamage();
+    }
+
+
+}
---
+++ src/gameRpg2/WeaponsApp/Sword.java
@@ -0,0 +1,21 @@
+package gameRpg2.WeaponsApp;
+
+public class Sword extends Weapons {
+
+
+        public Sword() {
+
+        }
+
+        public Sword(int weaponDamage) {
+            super(weaponDamage);
+        }
+
+        @Override
+        public int getWeaponDamage() {
+            return super.getWeaponDamage();
+        }
+
+
+
+}
