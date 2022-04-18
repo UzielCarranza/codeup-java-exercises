9706cb5c198a339c520a629e9e2176c460e59664
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-18 16:24:36 -0400

    initial code

---
+++ src/GradesApplication.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class GradesApplication {
+
+    public static void main(String[] args) {
+        HashMap <String, String > students = new HashMap<>();
+        students.put("GithubUsername:", String.valueOf(students));
+    }
+}
