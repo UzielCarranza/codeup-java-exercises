98496a5f3f48df7b07ec446506698fda1879b9ab
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-19 19:58:47 -0400

    exeption for number

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,45 +1,45 @@
 package shapes;
 
 import util.Input;
-import shapes.Circle;
-
-import java.text.DecimalFormat;
 
 public class CircleApp {
 
     public static void main(String[] args) {
-        boolean keepMakingCircle = true;
+//        boolean keepMakingCircle = true;
+//
+//        do {
+//            Input circleApp = new Input();
+//
+//            System.out.println("please enter a radius for your circle");
+//            double radius = circleApp.getDouble();
+//            Circle circle1 = new Circle(radius);
+//
+//            System.out.println("----------------Circle details---------------");
+//            System.out.println("Area: " + circle1.getArea());
+//            System.out.println("Circumference: " + circle1.getCircumference());
+//
+//
+//            DecimalFormat dfArea = new DecimalFormat("###.##");
+//            System.out.println("Rounded value (DecimalFormat): " + dfArea.format(circle1.getArea()));
+//
+//            DecimalFormat dfCircumference = new DecimalFormat("###.##");
+//            System.out.println("Rounded value (DecimalFormat): " + dfCircumference.format(circle1.getCircumference()));
+//
+//            Input input = new Input();
+//            System.out.println("----------------System off---------------");
+//            System.out.println("cotinue making a circle?");
+//            if (input.yesNo()) {
+//                keepMakingCircle = true;
+//
+//                System.out.println("----------------System on---------------");
+//            } else {
+//
+//                System.out.println("----------------Shutting dowm---------------");
+//                keepMakingCircle = false;
+//            }
+//        } while (keepMakingCircle);
+        Input input = new Input();
 
-        do {
-            Input circleApp = new Input();
-
-            System.out.println("please enter a radius for your circle");
-            double radius = circleApp.getDouble();
-            Circle circle1 = new Circle(radius);
-
-            System.out.println("----------------Circle details---------------");
-            System.out.println("Area: " + circle1.getArea());
-            System.out.println("Circumference: " + circle1.getCircumference());
-
-
-            DecimalFormat dfArea = new DecimalFormat("###.##");
-            System.out.println("Rounded value (DecimalFormat): " + dfArea.format(circle1.getArea()));
-
-            DecimalFormat dfCircumference = new DecimalFormat("###.##");
-            System.out.println("Rounded value (DecimalFormat): " + dfCircumference.format(circle1.getCircumference()));
-
-            Input input = new Input();
-            System.out.println("----------------System off---------------");
-            System.out.println("cotinue making a circle?");
-            if (input.yesNo()) {
-                keepMakingCircle = true;
-
-                System.out.println("----------------System on---------------");
-            } else {
-
-                System.out.println("----------------Shutting dowm---------------");
-                keepMakingCircle = false;
-            }
-        } while (keepMakingCircle);
+        input.getInt();
     }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,9 +1,6 @@
 package util;
 
-import java.util.Locale;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Input {
     //    fields
@@ -20,7 +17,7 @@ public class Input {
     }
 
     String getString() {
-        System.out.println("Enter yes or no");
+        System.out.println("Enter a number");
         return this.scanner.nextLine();
     }
 
@@ -34,44 +31,53 @@ public class Input {
 
 //    methods for numbers
 
-    int getInt(int min, int max) {
-        this.min = min;
-        this.max = max;
-        if (min < max && min >= 0) {
-            System.out.println("Great Selection!!");
-        } else {
-            return getInt();
+//    int getInt(int min, int max) {
+//        this.min = min;
+//        this.max = max;
+//        if (min < max && min >= 0) {
+//            System.out.println("Great Selection!!");
+//        } else {
+//            return getInt();
+//        }
+//        return min;
+//    }
+//
+//    public int getInt() {
+//
+//        System.out.println("Enter a number between 0 and 5");
+//        this.min = scanner.nextInt();
+//
+//        getInt(this.min, this.max);
+//        return this.min;
+//    }
+//
+//    double getDouble(double min, double max) {
+//        this.minDouble = min;
+//        this.maxDouble = max;
+//        if (min < max && min > 0) {
+//            System.out.println("Within Range");
+//        } else {
+//            System.out.println("needs number between 1 and 20");
+//            return getDouble();
+//        }
+//        return min;
+//
+//    }
+//
+//    public double getDouble() {
+//        System.out.println("Enter a number between 1 and 20");
+//        this.minDouble = scanner.nextDouble();
+//        getDouble(this.minDouble, this.max);
+//        return this.minDouble;
+//    }
+    public int getInt(){
+        String inputUser = getString();
+        try{
+            return Integer.parseInt( inputUser);
+        } catch (NumberFormatException e){
+            System.out.println(e);
         }
-        return min;
-    }
-
-    public int getInt() {
-
-        System.out.println("Enter a number between 0 and 5");
-        this.min = scanner.nextInt();
-
-        getInt(this.min, this.max);
-        return this.min;
-    }
-
-    double getDouble(double min, double max) {
-        this.minDouble = min;
-        this.maxDouble = max;
-        if (min < max && min > 0) {
-            System.out.println("Within Range");
-        } else {
-            System.out.println("needs number between 1 and 20");
-            return getDouble();
-        }
-        return min;
-
-    }
-
-    public double getDouble() {
-        System.out.println("Enter a number between 1 and 20");
-        this.minDouble = scanner.nextDouble();
-        getDouble(this.minDouble, this.max);
-        return this.minDouble;
+        return getInt();
     }
 
 
