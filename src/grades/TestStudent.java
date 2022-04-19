e986ec828b23613cd98a01466f1bd12794af0788
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-18 22:01:34 -0400

    completed execise

--- src/grades/TestStudent.java
+++ src/grades/TestStudent.java
@@ -1,12 +1,13 @@
 package grades;
 
-public class Studens {
+public class TestStudent {
 
     public static void main(String[] args) {
         Student student = new Student("josh");
         student.addGrade(8);
         student.addGrade(16);
         student.addGrade(45);
+        student.getGradeAverage();
         System.out.println(student.getGradeAverage());
     }
 }
