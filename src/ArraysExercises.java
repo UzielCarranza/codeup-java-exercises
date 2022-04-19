a5291bc4e64d40e9b93b457cd488ff39abac2b38
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-19 13:03:20 -0400

    fixed exercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -6,23 +6,46 @@ public class ArraysExercises {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
 
-        String[] Person = {"John", "Paul", "Ringo" };
+        String[] Person = {"John", "Paul", "Ringo"};
         for (String person : Person) {
             System.out.println(person);
         }
-        System.out.println(Arrays.toString(addPerson(Person, "one")));
+//        System.out.println(Arrays.toString(addPerson(Person, "one")));
 
+        Person[] people = new Person[3];
+        people[0] = new Person("john");
+        people[1] = new Person("joh");
+        people[2] = new Person("jo");
 
-    }
-    public static String [] addPerson(String [] Person, String person){
-
-        String[] personCopy = Arrays.copyOf(Person, Person.length + 1);
-
-        for (int i =0; i < Person.length; i++){
-            personCopy[personCopy.length -1] = person;
+//        for (Person person : people) {
+//            person.getName();
+//            System.out.println(person.getName());
+//        }
+        people = addPerson(people, new Person("new"));
+        people = addPerson(people, new Person("aa"));
+        for (Person person : people){
+            System.out.println(person.getName());
         }
-
-        return personCopy;
     }
+
+    public static Person[] addPerson(Person[] personArr, Person personToAdd) {
+        Person[] newArr = Arrays.copyOf(personArr, personArr.length + 1);
+
+        newArr[newArr.length - 1] = personToAdd;
+        return newArr;
+
+    }
+
+//    public static String[] addPerson(String[] Person, String person) {
+//
+//        String[] personCopy = Arrays.copyOf(Person, Person.length + 1);
+//
+//        for (int i = 0; i < Person.length; i++) {
+//            personCopy[personCopy.length - 1] = person;
+//        }
+//
+//        return personCopy;
+//    }
+
 
 }
