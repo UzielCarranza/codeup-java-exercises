dada46dabe10b7e5bbfa78425162a24c57a9a8f0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-18 20:49:00 -0400

    completed exercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -10,11 +10,19 @@ public class ArraysExercises {
         for (String person : Person) {
             System.out.println(person);
         }
+        System.out.println(Arrays.toString(addPerson(Person, "one")));
 
 
     }
-//    public static void addPerson(String [] Person){
-//
-//    }
+    public static String [] addPerson(String [] Person, String person){
+
+        String[] personCopy = Arrays.copyOf(Person, Person.length + 1);
+
+        for (int i =0; i < Person.length; i++){
+            personCopy[personCopy.length -1] = person;
+        }
+
+        return personCopy;
+    }
 
 }
