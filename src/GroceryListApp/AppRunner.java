19c7bb24e20268ae5f07febd049848211ab45e61
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-19 09:41:23 -0400

    getters and setters

--- src/GroceryListApp/AppRunner.java
+++ src/GroceryListApp/AppRunner.java
@@ -5,5 +5,6 @@ public class AppRunner {
 
     public static void main(String[] args) {
 
+
     }
 }
--- src/GroceryListApp/GroceryList.java
+++ src/GroceryListApp/GroceryList.java
@@ -1,4 +1,39 @@
 package GroceryListApp;
 
+import java.util.HashMap;
+
 public class GroceryList {
+    HashMap list;
+    String item;
+    int itemQuantity;
+
+
+
+    public GroceryList(){
+
+    }
+
+    public HashMap getList() {
+        return list;
+    }
+
+    public void setList(HashMap list) {
+        this.list = list;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
 }
