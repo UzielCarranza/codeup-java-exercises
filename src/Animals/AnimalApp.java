4252c864a888d0df1308ee453b9adc487e4e6467
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-20 10:27:41 -0400

    completed warm up

--- src/Animals/AnimalApp.java
+++ src/Animals/AnimalApp.java
@@ -9,11 +9,19 @@ public class AnimalApp {
         int numLegs = 4;
 
         String prefClimate = "in the house";
-
         Animal animal = new Dog();
         animal.setNumberOFlegs(numLegs);
         animal.setPreferredClimate(prefClimate);
         System.out.println(animal.getAnimalInfo());
 
+
+        System.out.println("-----------------");
+        Animal fish = new Fish(1,0, "water");
+        System.out.println(fish.getAnimalInfo());
+
+
+        System.out.println("-----------------");
+        Animal cat = new Cat(4, "In the house");
+        System.out.println(cat.getAnimalInfo());
     }
 }
