2a04c0458f0617efb56c67d37f7080930ba3aa94
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-20 10:36:51 -0400

    added colors

--- src/Animals/AnimalApp.java
+++ src/Animals/AnimalApp.java
@@ -12,16 +12,16 @@ public class AnimalApp {
         Animal animal = new Dog();
         animal.setNumberOFlegs(numLegs);
         animal.setPreferredClimate(prefClimate);
-        System.out.println(animal.getAnimalInfo());
+        System.out.println("\033[0;92m" + animal.getAnimalInfo());
 
 
         System.out.println("-----------------");
         Animal fish = new Fish(1,0, "water");
-        System.out.println(fish.getAnimalInfo());
+        System.out.println("\033[0;94m" + fish.getAnimalInfo());
 
 
         System.out.println("-----------------");
         Animal cat = new Cat(4, "In the house");
-        System.out.println(cat.getAnimalInfo());
+        System.out.println("\033[1;93m" +cat.getAnimalInfo());
     }
 }
