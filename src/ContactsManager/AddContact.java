fc37ed6e6ebbd9fbd608a495718b26e1de955a4d
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-21 16:19:05 -0400

    set up adding contacts in clase

--- src/ContactsManager/AddContact.java
+++ src/ContactsManager/AddContact.java
@@ -1,18 +1,24 @@
-//package ContactsManager;
-//
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.nio.file.StandardOpenOption;
-//import java.util.Arrays;
-//
-//public class AddContact {
-//
-//
-//        public AddContact(){
-//            Files.write(
-//                    Paths.get("Contacts", "Contacts.txt"),
-//                    Arrays.asList(addContacts.getContactName(), addContacts.getPhoneMumber()), // list with one item
-//                    StandardOpenOption.APPEND);
-//        }
-//
-//}
+package ContactsManager;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+
+public class AddContact {
+    private String contactName;
+    private String phoneNumber;
+
+    public AddContact(String contactName, String phoneNumber) {
+        try {
+            Files.write(
+                    Paths.get("Contacts", "Contacts.txt"),
+                    Arrays.asList(contactName, phoneNumber), // list with one item
+                    StandardOpenOption.APPEND);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+}
